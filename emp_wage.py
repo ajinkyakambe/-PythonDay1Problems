b5174a55d946010113d143118f19f264c5ee1f3e
ajinkyakambe <ajinkyakambe@gmail.com>
2022-08-10 02:30:40 +0530

    Calculating part time wage

--- emp_wage.py
+++ emp_wage.py
@@ -8,11 +8,18 @@ def wage_computation():
     """
     employee_wage_per_hour = 20
     is_full_time = 1
+    is_part_time = 2
     full_time_hour = 8
-    employee_status = random.randint(0, 1)
+    part_time_hour = 4
+    employee_status = random.randint(0, 2)
+
     if employee_status == is_full_time:
-        print("Employee is Present")
+        print("Employee is Present for a Full Day")
         employee_wage = full_time_hour * employee_wage_per_hour
+        print("Employee Wage for a Day: ", employee_wage)
+    elif employee_status == is_part_time:
+        print("Employee is Present for a Half Day")
+        employee_wage = part_time_hour * employee_wage_per_hour
         print("Employee Wage for a Day: ", employee_wage)
     else:
         print("Employee is Absent")
