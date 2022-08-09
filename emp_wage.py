583c56c748482503291de0f941f54ff9ec1a9c47
ajinkyakambe <ajinkyakambe@gmail.com>
2022-08-10 02:28:59 +0530

    Calculating daily employee wage

--- emp_wage.py
+++ emp_wage.py
@@ -1,7 +1,6 @@
 #Library for random func
 import random
 
-
 def wage_computation():
     """
     This function computes wage of an employee
@@ -9,18 +8,11 @@ def wage_computation():
     """
     employee_wage_per_hour = 20
     is_full_time = 1
-    is_part_time = 2
     full_time_hour = 8
-    part_time_hour = 4
-    employee_status = random.randint(0, 2)
-
+    employee_status = random.randint(0, 1)
     if employee_status == is_full_time:
-        print("Employee is Present for a Full Day")
+        print("Employee is Present")
         employee_wage = full_time_hour * employee_wage_per_hour
-        print("Employee Wage for a Day: ", employee_wage)
-    elif employee_status == is_part_time:
-        print("Employee is Present for a Half Day")
-        employee_wage = part_time_hour * employee_wage_per_hour
         print("Employee Wage for a Day: ", employee_wage)
     else:
         print("Employee is Absent")
